cc63c268cb96870c218e78d0367e961fdf794a8f
AmruthaMishra <amruthamishra9099@gmail.com>
2020-08-25 16:47:47 -0400

    Update Patientcontroller.java

--- src/main/java/com/healthcare/healthcare/Patientcontroller.java
+++ src/main/java/com/healthcare/healthcare/Patientcontroller.java
@@ -17,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class Patientcontroller {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(Patientcontroller.class);
